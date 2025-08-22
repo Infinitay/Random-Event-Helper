5d09c85904dd8c9cc0c1f229711462ee08b2ce9c
Infinitay <Infinitay@users.noreply.github.com>
2025-08-22 12:33:58 -0400

    feat: Add support for the Beekeeper random event

--- src/main/java/randomeventsolver/RandomEventSolverPlugin.java
+++ src/main/java/randomeventsolver/RandomEventSolverPlugin.java
@@ -1,13 +1,19 @@
 package randomeventsolver;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
 import com.google.inject.Provides;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import javax.inject.Inject;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -65,6 +71,9 @@ public class RandomEventSolverPlugin extends Plugin
 	private Widget patternNextAnswerWidget;
 
 	private OSRSItemRelationshipSystem relationshipSystem;
+
+	@Getter
+	private ImmutableList<Widget> beehiveAnswerWidgets;
 
 	private static final int[] PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_MODELS = {
 		InterfaceID.PatternCards.CARD_0,
@@ -138,7 +147,8 @@ public class RandomEventSolverPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameTick(GameTick gameTick) {
+	public void onGameTick(GameTick gameTick)
+	{
 
 	}
 
@@ -226,6 +236,83 @@ public class RandomEventSolverPlugin extends Plugin
 				}
 			});
 		}
+
+		if (widgetLoaded.getGroupId() == InterfaceID.BEEHIVE)
+		{
+			this.clientThread.invokeLater(() -> {
+				Widget exampleHiveWidget = this.client.getWidget(InterfaceID.Beehive.EXAMPLE);
+				if (exampleHiveWidget != null)
+				{
+					// Number the placeholder texts to help users
+					Widget destination1LayerWidget = this.client.getWidget(InterfaceID.Beehive.UNIVERSE_TEXT8);
+					Widget destination2LayerWidget = this.client.getWidget(InterfaceID.Beehive.UNIVERSE_TEXT10);
+					Widget destination3LayerWidget = this.client.getWidget(InterfaceID.Beehive.UNIVERSE_TEXT12);
+					Widget destination4LayerWidget = this.client.getWidget(InterfaceID.Beehive.UNIVERSE_TEXT14);
+					if (destination1LayerWidget != null && destination2LayerWidget != null && destination3LayerWidget != null && destination4LayerWidget != null)
+					{
+						destination1LayerWidget.setText("1. " + destination1LayerWidget.getText());
+						destination2LayerWidget.setText("2. " + destination2LayerWidget.getText());
+						destination3LayerWidget.setText("3. " + destination3LayerWidget.getText());
+						destination4LayerWidget.setText("4. " + destination4LayerWidget.getText());
+					}
+					else
+					{
+						log.warn("One or more beehive destination layer widgets are null.");
+					}
+
+					// Lid model ID = 28806, Body model ID = 28428, entrance layer model ID = 28803, legs layer model ID = 28808
+					// The following widgets are the initial (jumbled) layers of the beehive that we have to rearrange
+					Widget start1LayerWidget = this.client.getWidget(InterfaceID.Beehive.START_1);
+					Widget start2LayerWidget = this.client.getWidget(InterfaceID.Beehive.START_2);
+					Widget start3LayerWidget = this.client.getWidget(InterfaceID.Beehive.START_3);
+					Widget start4LayerWidget = this.client.getWidget(InterfaceID.Beehive.START_4);
+					if (start1LayerWidget != null && start2LayerWidget != null && start3LayerWidget != null && start4LayerWidget != null)
+					{
+						int start1ModelID = start1LayerWidget.getModelId();
+						int start2ModelID = start2LayerWidget.getModelId();
+						int start3ModelID = start3LayerWidget.getModelId();
+						int start4ModelID = start4LayerWidget.getModelId();
+						log.debug("Beehive start layer model IDs: {}, {}, {}, {}", start1ModelID, start2ModelID, start3ModelID, start4ModelID);
+						// Use this set as the correct order of the beehive layers from top to bottom (Lid, Body, Entrance, Legs)
+						BiMap<Widget, Integer> startingLayerMap = ImmutableBiMap.<Widget, Integer>builder()
+							.put(start1LayerWidget, start1ModelID)
+							.put(start2LayerWidget, start2ModelID)
+							.put(start3LayerWidget, start3ModelID)
+							.put(start4LayerWidget, start4ModelID)
+							.build();
+						Widget[] correctBeehiveOrderWidgets = new Widget[4];
+						for (Integer modelID : startingLayerMap.values())
+						{
+							switch (modelID)
+							{
+								case 28806: // Lid
+									correctBeehiveOrderWidgets[0] = startingLayerMap.inverse().get(28806);
+									break;
+								case 28428: // Body
+									correctBeehiveOrderWidgets[1] = startingLayerMap.inverse().get(28428);
+									break;
+								case 28803: // Entrance
+									correctBeehiveOrderWidgets[2] = startingLayerMap.inverse().get(28803);
+									break;
+								case 28808: // Legs
+									correctBeehiveOrderWidgets[3] = startingLayerMap.inverse().get(28808);
+									break;
+								default:
+									log.warn("Unexpected beehive layer model ID: {}", modelID);
+									break;
+							}
+						}
+						this.beehiveAnswerWidgets = ImmutableList.copyOf(correctBeehiveOrderWidgets);
+						log.debug("Correct beehive order widgets: {}", this.beehiveAnswerWidgets);
+					}
+					else
+					{
+						log.warn("One or more beehive start layer widgets are null.");
+						this.beehiveAnswerWidgets = null;
+					}
+				}
+			});
+		}
 	}
 
 	@Subscribe
@@ -244,6 +331,12 @@ public class RandomEventSolverPlugin extends Plugin
 			log.debug("Pattern next widget closed, resetting pattern next answer.");
 			this.patternNextAnswer = null;
 			this.patternNextAnswerWidget = null;
+		}
+
+		if (widgetClosed.getGroupId() == InterfaceID.BEEHIVE)
+		{
+			log.debug("Beehive widget closed, resetting beehive answer widgets.");
+			this.beehiveAnswerWidgets = null;
 		}
 	}
 
@@ -316,12 +409,15 @@ public class RandomEventSolverPlugin extends Plugin
 		return null;
 	}
 
-	public Widget getPatternCardSelectionWidgetFromModel(Widget modelWidget) {
+	public Widget getPatternCardSelectionWidgetFromModel(Widget modelWidget)
+	{
 		// This method retrieves the widget for the pattern card selection based on the model widget.
 		// PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_MODELS[i] = PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_SELECTS[i]
 		// So if modelWidget corresponds to PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_MODELS[i] then it should return PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_SELECTS[i]
-		for (int i = 0; i < PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_MODELS.length; i++) {
-			if (modelWidget.getId() == PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_MODELS[i]) {
+		for (int i = 0; i < PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_MODELS.length; i++)
+		{
+			if (modelWidget.getId() == PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_MODELS[i])
+			{
 				return this.client.getWidget(PATTERNCARDS_INTERFACEIDS_AVAILABLE_CARD_SELECTS[i]);
 			}
 		}
