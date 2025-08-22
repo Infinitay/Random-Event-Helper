5d3e0151f99167af429bf08bcfdd0e53b98ab2c6
Infinitay <Infinitay@users.noreply.github.com>
2025-08-22 12:33:58 -0400

    feat: Add support for the Beekeeper random event

--- src/main/java/randomeventsolver/RandomEventSolverOverlay.java
+++ src/main/java/randomeventsolver/RandomEventSolverOverlay.java
@@ -5,12 +5,16 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 import javax.inject.Inject;
 import net.runelite.api.Client;
+import net.runelite.api.Perspective;
+import net.runelite.api.Point;
+import net.runelite.api.coords.LocalPoint;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayUtil;
 import net.runelite.client.ui.overlay.outline.ModelOutlineRenderer;
+import net.runelite.client.util.Text;
 
 public class RandomEventSolverOverlay extends Overlay
 {
@@ -34,10 +38,10 @@ public class RandomEventSolverOverlay extends Overlay
 	{
 		if (plugin.getPatternCardAnswerWidgets() != null && !plugin.getPatternCardAnswerWidgets().isEmpty())
 		{
-			for (Widget answerWidget : plugin.getPatternCardAnswerWidgets()) {
+			for (Widget answerWidget : plugin.getPatternCardAnswerWidgets())
+			{
 				if (answerWidget != null && !answerWidget.isHidden())
 				{
-					Widget parentWidget = answerWidget.getParent();
 					OverlayUtil.renderPolygon(graphics2D, answerWidget.getBounds(), Color.GREEN);
 				}
 			}
@@ -46,8 +50,27 @@ public class RandomEventSolverOverlay extends Overlay
 
 		}
 
-		if (plugin.getPatternNextAnswer() != null) {
+		if (plugin.getPatternNextAnswer() != null)
+		{
 			OverlayUtil.renderPolygon(graphics2D, plugin.getPatternNextAnswerWidget().getBounds(), Color.GREEN);
+		}
+
+		if (plugin.getBeehiveAnswerWidgets() != null && !plugin.getBeehiveAnswerWidgets().isEmpty())
+		{
+			for (int i = 0; i < plugin.getBeehiveAnswerWidgets().size(); i++)
+			{
+				Widget answerWidget = plugin.getBeehiveAnswerWidgets().get(i);
+				if (answerWidget != null && !answerWidget.isHidden() && answerWidget.getModelId() != -1)
+				{
+					graphics2D.setFont(graphics2D.getFont().deriveFont(18f));
+					String text = String.valueOf(i + 1);
+					Point textPoint = new Point(
+						answerWidget.getBounds().x + (answerWidget.getBounds().width / 2) - (graphics2D.getFontMetrics().stringWidth(text) / 2),
+						answerWidget.getBounds().y + (answerWidget.getBounds().height / 2) + (graphics2D.getFontMetrics().getHeight() / 2) - graphics2D.getFontMetrics().getDescent()
+					);
+					OverlayUtil.renderTextLocation(graphics2D, textPoint, text, Color.YELLOW);
+				}
+			}
 		}
 
 		return null;
