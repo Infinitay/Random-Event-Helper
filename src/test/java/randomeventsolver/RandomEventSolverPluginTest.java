0a184ac27bab6a9af398860414841e4ecec6aee7
Infinitay <Infinitay@users.noreply.github.com>
2025-08-22 11:31:18 -0400

    feat: Add support for the Suprise Exam random event

--- src/test/java/randomeventsolver/RandomEventSolverPluginTest.java
+++ src/test/java/randomeventsolver/RandomEventSolverPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package randomeventsolver;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RandomEventSolverPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(RandomEventSolverPlugin.class);
 		RuneLite.main(args);
 	}
 }
