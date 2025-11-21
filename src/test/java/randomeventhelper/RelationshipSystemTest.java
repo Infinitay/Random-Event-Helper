d36fdee76de5cfbbb7bdaadad96de72c10bc9a95
Infinitay <Infinitay@users.noreply.github.com>
2025-11-21 05:30:51 -0500

    test(surpriseexam): Initiated unit testing for Surprise Exam (OSRSItemRelationshipSystem)

---
+++ src/test/java/randomeventhelper/RelationshipSystemTest.java
@@ -0,0 +1,17 @@
+package randomeventhelper;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RelationshipSystemTest
+{
+	@Test
+	public void testPatternMatching() {
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testNextMissingItem() {
+		Assert.assertTrue(true);
+	}
+}
